0a7f6879e4b6c66a7b917ac1435f4acc8c845155
BLFletcher23 <bl.fletcher98@gmail.com>
2023-11-22 00:56:31 -0600

    card and weapon

--- game/Weapon.java
+++ game/Weapon.java
@@ -1,5 +1,38 @@
 package game;
 
+import game.Room;
 public class Weapon {
+	Room room;
+	private String weaponName;
+	private int xCoord;
+	private int yCoord;
 
+	
+	public void setWeaponName(String weaponName)
+	{
+		this.weaponName = weaponName;
+	}
+	public String getWeaponName()
+	{
+		return weaponName;
+	}
+	public void setXcoord(int xCoord)
+	{
+		this.xCoord = xCoord;
+		
+	}
+	public int getXcoord()
+	{
+		return xCoord;
+	}
+	public void setYoord(int yCoord)
+	{
+		this.yCoord = yCoord;
+		
+	}
+	public boolean isInRoom(String roomName)
+	{
+		return false;
+	}
 }
+
