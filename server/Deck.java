4647c955a378033a05b252f45ad6d77423291d5e
ChadRich <chickmagnet50@yahoo.com>
2023-11-30 17:19:20 -0600

    Changed Strings to Objects in Deck File
    Revamped the Deck file to include objects instead of strings so that they can be passed back and forth in the game.

--- server/Deck.java
+++ server/Deck.java
@@ -4,92 +4,132 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Deck {
-	//this is how we want to change these
-//	  private final List<Room> rooms;
-//    private final List<Player> suspects;
-//    private final List<Weapon> weapons;
-//    private final List<String> fullDeck;
+
     
-    private final List<String> rooms;
-    private final List<String> suspects;
-    private final List<String> weapons;
-    private final List<String> fullDeck;
+public class Deck {
+
+    
+    private List<Room> rooms;
+    private List<Suspect> suspects;
+    private List<Weapon> weapons;
+    private List<Object> fullDeck;
 
     // Constructor to initialize the lists
     public Deck() {
-        this.rooms = new ArrayList<>();
-        this.suspects = new ArrayList<>();
-        this.fullDeck = new ArrayList<>();
-        this.weapons = new ArrayList<>();
+    	this.rooms = new ArrayList<>();
+    	this.suspects = new ArrayList<>();
+    	this.fullDeck = new ArrayList<>();
+    	this.weapons = new ArrayList<>();
     }
 
     // Function to categorize cards into rooms, suspects, and weapons
     public void categorizeCards() {
-       
-        rooms.add("Kitchen");
-        rooms.add("Ballroom");
-        rooms.add("Conservatory");
-        rooms.add("Dining Room");
-        rooms.add("Billiards Room");
-        rooms.add("Lounge");
-        rooms.add("Hall");
-        rooms.add("Study");
+
+    	rooms.add(new Room("Kitchen"));
+    	rooms.add(new Room("Ballroom"));
+    	rooms.add(new Room("Conservatory"));
+    	rooms.add(new Room("Dining Room"));
+    	rooms.add(new Room("Billiards Room"));
+    	rooms.add(new Room("Lounge"));
+    	rooms.add(new Room("Hall"));
+    	rooms.add(new Room("Study"));
 
 
-        suspects.add("Colonel Mustard");
-        suspects.add("Miss Scarlet");
-        suspects.add("Mrs. Peacock");
-        suspects.add("Dr. Orchid");
-        suspects.add("Professor Plum");
-        suspects.add("Reverend Green");
-      
+    	suspects.add(new Suspect("Colonel Mustard"));
+    	suspects.add(new Suspect("Miss Scarlet"));
+    	suspects.add(new Suspect("Mrs. Peacock"));
+    	suspects.add(new Suspect("Dr. Orchid"));
+    	suspects.add(new Suspect("Professor Plum"));
+    	suspects.add(new Suspect("Reverend Green"));
 
-        weapons.add("Candlestick");
-        weapons.add("Revolver");
-        weapons.add("Dagger");
-        weapons.add("Lead Pipe");
-        weapons.add("Wrench");
-        weapons.add("Rope");
-  
 
-        // Combine all cards into the fullDeck
-        fullDeck.addAll(rooms);
-        fullDeck.addAll(suspects);
-        fullDeck.addAll(weapons);
+    	weapons.add(new Weapon("Candlestick"));
+    	weapons.add(new Weapon("Revolver"));
+    	weapons.add(new Weapon("Dagger"));
+    	weapons.add(new Weapon("Lead Pipe"));
+    	weapons.add(new Weapon("Wrench"));
+    	weapons.add(new Weapon("Rope"));
+
+
+    	// Combine all cards into the fullDeck
+    	fullDeck.addAll(rooms);
+    	fullDeck.addAll(suspects);
+    	fullDeck.addAll(weapons);
     }
+
 
     // Function to shuffle the full deck
     public void shuffle() {
-        Collections.shuffle(fullDeck);
+    	Collections.shuffle(fullDeck);
     }
 
     // Getter functions for rooms, suspects, fullDeck, and weapons
-    public List<String> getRooms() {
-        return rooms;
+    public List<Room> getRooms() {
+    	return rooms;
     }
 
-    public List<String> getSuspects() {
-        return suspects;
+    public List<Suspect> getSuspects() {
+    	return suspects;
     }
 
-    public List<String> getFullDeck() {
-        return fullDeck;
+    public List<Weapon> getWeapons() {
+    	return weapons;
     }
 
-    public List<String> getWeapons() {
-        return weapons;
+    public List<Object> getFullDeck() {
+    	return fullDeck;
     }
 
+    static class Room {
+        private String name;
+
+        public Room(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    static class Suspect {
+        private String name;
+
+        public Suspect(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    static class Weapon {
+        private String name;
+
+        public Weapon(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+        
+
+}
+    
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.categorizeCards();
-        deck.shuffle();
+    	Deck deck = new Deck();
+    	deck.categorizeCards();
+    	deck.shuffle();
 
-        // Example usage of getter functions
-        System.out.println("Rooms: " + deck.getRooms());
-        System.out.println("Suspects: " + deck.getSuspects());
-        System.out.println("Weapons: " + deck.getWeapons());
-        System.out.println("Full Deck: " + deck.getFullDeck());
+    	// Example usage of getter functions
+    	System.out.println("Rooms: " + deck.getRooms());
+    	System.out.println("Suspects: " + deck.getSuspects());
+    	System.out.println("Weapons: " + deck.getWeapons());
+    	System.out.println("Full Deck: " + deck.getFullDeck());
     }
 }
