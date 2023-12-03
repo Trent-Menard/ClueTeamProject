b1dc1d09ac31aa8b9b8d1cae64aaad993231e410
BLFletcher23 <bl.fletcher98@gmail.com>
2023-12-03 13:52:04 -0600

    Randomized WeaponCoords

--- game/BoardController.java
+++ game/BoardController.java
@@ -1,18 +1,28 @@
 package game;
 
 import clientCommunication.GameClient;
+import game.Room;
+import server.Deck;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class BoardController implements ActionListener {
     private BoardPanel boardPanel;
     private GameClient client;
+    private Weapon weapon;
+    private Room room;
+    private Deck deck;
+    Random rand = new Random();
+    
     private int row;
     private int col;
-
+    private int[] WeaponPointsX = {1,2,1,7,7,13,12,13};
+    private int[] WeaponPointsY = {2,7,12,2,12,2,7,12};
+    	
     public BoardController(BoardPanel boardPanel) {
         this.boardPanel = boardPanel;
     }
@@ -26,6 +36,16 @@ public class BoardController implements ActionListener {
     public BoardController(BoardPanel bp, GameClient client) {
         this(bp);
         this.client = client;
+    }
+    public void randomizeWeapons(Weapon weapon)
+    {
+    	for (int i= 0; i < 6 ; i++)
+    	{
+    		int randX = rand.nextInt(WeaponPointsX.length);
+    		int randY = rand.nextInt(WeaponPointsY.length);
+    		weapon.setXcoord(randX);
+    		weapon.setYcoord(randY);
+    	}
     }
 
     @Override
