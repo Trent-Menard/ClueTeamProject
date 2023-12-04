2bc09c9ae83a6b7c004a13a9343bc00e7dca31d7
rrooke14 <rrooke@cub.uca.edu>
2023-12-03 18:20:43 -0600

    Waiting room button renamed
    I don't know how to implement the panel. We can put the panel itself inside of a layered pane section OR if we have to we could do a separate card in the cardlayout and just not have the board visible during accusation

--- client/AccusationPanel.java
+++ client/AccusationPanel.java
@@ -1,7 +1,19 @@
 package client;
 
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class AccusationPanel extends JPanel{
 	
+	
+	public AccusationPanel(/*some sort of controller*/) {
+		JPanel choicePanel = new JPanel(new GridLayout(1, 3));
+		JComboBox suspects = new JComboBox();
+		JComboBox weapons = new JComboBox();
+		JComboBox rooms = new JComboBox();
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+		JButton confirm = new JButton("Confirm");
+		JButton cancel = new JButton("Cancel");
+	}
 }
