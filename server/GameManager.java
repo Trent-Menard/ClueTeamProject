ae85c90911ac654cd57a80d4326215b08dd85cbb
ChadRich <chickmagnet50@yahoo.com>
2023-12-04 00:39:17 -0600

    Fixing Conflicts
    We should be good to go

--- server/GameManager.java
+++ server/GameManager.java
@@ -1,16 +1,22 @@
 package server;
 
-import java.util.Collections;
-
 import game.Player;
+import game.Suspect;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GameManager {
     private final Deck deck;
-   
+    private int playersReady = 0;
+    private int numOfPlayersNeededToStart;
+    private final List<Player> players = new ArrayList<>();
+    private List<Suspect> suspects = new ArrayList<>();
 
     public GameManager() {
         this.deck = new Deck();
         this.deck.categorizeCards();
+        this.suspects = deck.getSuspects();
     }
 
     public void assignPlayerDeck(Player player) {
